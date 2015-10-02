9d677122d9c2ee2d690cca420c612158484a5627
Addison Correll <acorrell5106@stu.neisd.net>
2015-10-02 07:24:57 -0500

    challenge 0

--- src/correll_4_challenge0/Correll_4_challenge0.java
+++ src/correll_4_challenge0/Correll_4_challenge0.java
@@ -17,23 +17,23 @@ public class Correll_4_challenge0 {
     public static void main(String[] args) {
         // TODO code application logic here
         for (int i = 1; i < 501; i++) {
-            if (isD11(i) & isD5(i)) {
+            if (isDivisibleby11(i) & isDivisibleby5(i)) {
                 System.out.println(i + " = Animate This!");
-            } else if (isD11(i) & isD3(i)) {
+            } else if (isDivisibleby11(i) & isDivisibleby3(i)) {
                 System.out.println(i + " = Herr Direktor");
-            } else if (isD11(i) & isD2(i)) {
+            } else if (isDivisibleby11(i) & isDivisibleby2(i)) {
                 System.out.println(i + " = ninja");
-            } else if (isD2(i) & isD3(i) & isD5(i)) {
+            } else if (isDivisibleby2(i) & isDivisibleby3(i) & isDivisibleby5(i)) {
                 System.out.println(i + " = Team Building");
-            } else if (isD3(i) & isD5(i)) {
+            } else if (isDivisibleby3(i) & isDivisibleby5(i)) {
                 System.out.println(i + " = Old School");
-            } else if (isD2(i) & isD3(i)) {
+            } else if (isDivisibleby2(i) & isDivisibleby3(i)) {
                 System.out.println(i + " = chisme");
-            } else if (isD5(i)) {
+            } else if (isDivisibleby5(i)) {
                 System.out.println(i + " = Vidal");
-            } else if (isD3(i)) {
+            } else if (isDivisibleby3(i)) {
                 System.out.println(i + " = Claughton");
-            } else if (isD2(i)) {
+            } else if (isDivisibleby2(i)) {
                 System.out.println(i + " = Davis");
             } else {
                 System.out.println(i + " = is not divisible");
@@ -41,19 +41,19 @@ public class Correll_4_challenge0 {
         }
     }
 
-    static boolean isD2(int twonumber) {
+    static boolean isDivisibleby2(int twonumber) {
         return (twonumber % 2 == 0);
     }
 
-    static boolean isD3(int threenumber) {
+    static boolean isDivisibleby3(int threenumber) {
         return (threenumber % 3 == 0);
     }
 
-    static boolean isD5(int fivenumber) {
+    static boolean isDivisibleby5(int fivenumber) {
         return (fivenumber % 5 == 0);
     }
 
-    static boolean isD11(int elevennumber) {
+    static boolean isDivisibleby11(int elevennumber) {
         return (elevennumber % 11 == 0);
     }
 }
